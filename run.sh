ddff4f0d3002da0ab326d9d405dba0eefe7b412b
mir-of <mir-of@outlook.com>
2018-12-06 17:19:48 +0800

    add run script

---
+++ run.sh
@@ -0,0 +1,21 @@
+python src/train_softmax.py \
+--logs_base_dir ./output_dir/logs/facenet/ \
+--models_base_dir ./output_dir/models/facenet/ \
+--data_dir /dataset/casia-webface/align-raw \
+--image_size 160 \
+--model_def models.inception_resnet_v1 \
+--optimizer ADAM \
+--learning_rate -1 \
+--max_nrof_epochs 1 \
+--keep_probability 0.8 \
+--random_crop \
+--random_flip \
+--use_fixed_image_standardization \
+--learning_rate_schedule_file data/learning_rate_schedule_classifier_casia.txt \
+--weight_decay 5e-4 \
+--embedding_size 512 \
+--validation_set_split_ratio 0.05 \
+--validate_every_n_epochs 1 \
+--prelogits_norm_loss_factor 5e-4 \
+--epoch_size=1 \
+--batch_size=90 
