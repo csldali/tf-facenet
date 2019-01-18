0bc4684d7e29aecc765d8834b34e4485de984942
mir-of <mir-of@outlook.com>
2019-01-18 17:06:23 +0800

    add train & eval scripts

---
+++ qiao_script/run_all_train.sh
@@ -0,0 +1,27 @@
+python ../src/train_softmax.py \
+--data_dir /dataset/casia-webface/align-raw \
+--logs_base_dir ./output_dir/logs/facenet/ \
+--models_base_dir ./output_dir/models/facenet/ \
+--image_size 160 \
+--model_def models.inception_resnet_v1 \
+--optimizer ADAM \
+--learning_rate -1 \
+--max_nrof_epochs 150 \
+--keep_probability 0.8 \
+--random_crop \
+--random_flip \
+--use_fixed_image_standardization \
+--learning_rate_schedule_file ../data/learning_rate_schedule_classifier_casia.txt \
+--weight_decay 5e-4 \
+--embedding_size 512 \
+--validation_set_split_ratio 0 \
+--prelogits_norm_loss_factor 5e-4 \
+--epoch_size=1000 \
+--batch_size=90 \
+--gpu_memory_fraction=0.9 \
+--lfw_dir /dataset/lfw/lfw_mtcnnpy_160 \
+--lfw_distance_metric 1 \
+--lfw_use_flipped_images \
+--lfw_subtract_mean \
+--lfw_pairs ../data/pairs.txt
+
--- qiao_script/run_validate_on_lfw.sh
+++ qiao_script/run_validate_on_lfw.sh
@@ -1,9 +1,9 @@
 python ../src/validate_on_lfw.py \
 /dataset/lfw/lfw_mtcnnpy_160 \
-/dataset/models/tf-facenet/20180402-114759 \
+/home/qiaojing/git-repo/facenet/qiao_script/output_dir/models/facenet/20190117-215245 \
 --distance_metric 1 \
 --use_fixed_image_standardization \
 --subtract_mean \
 --lfw_pairs /dataset/lfw/lfw_mtcnnpy_160/pairs.txt \
---lfw_batch_size 1 \
---lfw_nrof_folds 2 
+--lfw_batch_size 60 \
+--lfw_nrof_folds 10 
